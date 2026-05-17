7a14a67521d2d7f7c86574d876a34c659142720d
Abhishek <greenworlda111@gmail.com>
2026-05-17 13:53:19 +0530

    Configure codebase for custom domain shiva-groups.com

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/shivagroup/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
